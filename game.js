a189c6c04bbf5da73c5668cd8182e3120762e57b
Dzaky Fatur Rahman <dzaky2636@gmail.com>
2023-04-01 12:16:59 +0700

    Update game.js

--- game.js
+++ game.js
@@ -1,15 +1,14 @@
 // Konfigurasi hewan
 const hewan = {
     level: 1,
-    makan: 0,
-    minum: 0,
-    tidur: 0,
-    obat: 0,
-    currStatus: "none"
+    makan: 100,
+    minum: 100,
+    tidur: 100,
+    obat: 100
 }
 
 // Simulasi jam
-let hour = 23;
+let hour = 23; 
 let minutes = 0;
 setInterval(function(){
     minutes += 30;
@@ -20,23 +19,35 @@ setInterval(function(){
     else{ printMinutes = minutes.toString(); } 
     if(hour < 10){ printHour = hour.toString().padStart(2, '0'); }
     else{ printHour = hour.toString(); } 
-    console.log(printHour + ":" + printMinutes);
-    
-    $("#progressBarMakan").children().attr("style", "width: " + ((100 * hour) / 24) + "%");
+    // console.log(printHour + ":" + printMinutes);
 }, 1000);
 
 // simulasi aktivitas
 setInterval(function(){
+    // makan
+    makanActivity();
+}, 2000);
 
-}, 1000);
+function makanActivity(){
+    if(makanState && hewan.makan <= 100){
+        hewan.makan += 5;
+        $("#progressBarMakan").children().attr("class", "progress-bar bg-success progress-bar-striped progress-bar-animated");
+    }else if(!makanState && hewan.makan > 0){
+        hewan.makan -= 5;
+        $("#progressBarMakan").children().attr("class", "progress-bar bg-success");  
+    }
+    $("#progressBarMakan").children().attr("style", "width: " + ((100 * hewan.makan) / 100) + "%");
+}
 
-// simulasi aktivitas
+// button makan
 activityActive = false;
 makanState = false;
 $("#buttonMakan").click(function(){
     if(makanState){
-        $("#buttonMakan").attr("class", "btn btn-warning"); makanState = false;
+        $("#buttonMakan").attr("class", "btn btn-warning"); 
+        makanState = false;
     }else{
-        $("#buttonMakan").attr("class", "btn btn-success"); makanState = true;
+        $("#buttonMakan").attr("class", "btn btn-success"); 
+        makanState = true;
     }
 });
